cb40f17147ff7adbd55f542dfd3969573b1f90a1
proehr <pauline.roehr@yahoo.de>
2023-05-06 10:40:08 +0200

    6-tunnel-digging: Add dig functionality to player
    * Add SFX and VFX to dig hit
    * Collision detection and animation usage will have to be changed once we introduce our own player character

---
+++ Assets/Features/Digging/Logic/Digger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Linq;
+using Common.Logic.Variables;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Features.Digging.Logic
+{
+    public class Digger : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+        [SerializeField] private BoolVariable isDigging;
+        [SerializeField] private BoolVariable canMove;
+
+        private int animIdDig;
+        private float digAnimationLength;
+
+        private void Awake()
+        {
+            animIdDig = Animator.StringToHash("Dig");
+            digAnimationLength = animator.runtimeAnimatorController.animationClips
+                .First(clip => clip.name == "Attack (1)").length;
+        }
+
+        private void OnDig(InputValue value)
+        {
+            if (canMove.Get() && !isDigging.Get())
+            {
+                isDigging.Set(value.isPressed);
+                animator.SetTrigger(animIdDig);
+                canMove.Set(false);
+                StartCoroutine(StopDig());
+            }
+        }
+
+        private IEnumerator StopDig()
+        {
+            yield return new WaitForSeconds(digAnimationLength);
+            isDigging.Set(false);
+            canMove.Set(true);
+        }
+    }
+}
